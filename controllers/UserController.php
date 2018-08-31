314bed14f6bf5753dde9cc48645f29e565eef2db
sunwei <sunwei@czxy.com>
2018-08-31 17:38:14 +0800

    '添加代码'

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,6 +1,9 @@
 <?php
 namespace controllers;
 
+// 引入模型类
+use models\User;
+
 class UserController
 {
     public function hello()
@@ -10,8 +13,13 @@ class UserController
         $name = $user->getName();
 
         // 加载视图
-        view('users.hello', {
+        view('users.hello', [
             'name' => $name
-        });
+        ]);
+    }
+
+    public function world()
+    {
+        echo 'world';
     }
 }
--- public/index.php
+++ public/index.php
@@ -6,18 +6,25 @@ define('ROOT', dir(__FILE__) . '/../');
 function autoload($class)
 {
     $path = str_repeat('\\', '/', $class);
+
     require(ROOT . $path . '.php');
 }
 spl_autoload_register('autoload');
 
-
+// 添加路由 ： 解析 URL 上的路径
 $userController = new controllers\UserController;
-$userController->hello();
+$userController->world();
 
 
-
-
-function view($a,$b)
+// 加载视图
+// 参数一、加载的视图的文件名
+// 参数二、向视图中传的数据
+function view($viewFileName, $data = [])
 {
+    extract($data);
     
+    $path = str_replace('.', '/', $viewFileName) . '.html';
+
+    // 加载视图
+    require(ROOT . 'views/' . $path);
 }
