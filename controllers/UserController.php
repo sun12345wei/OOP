1e5749eea61ab98b7d6ceffd20414e26f0986876
sunwei <sunwei@czxy.com>
2018-09-01 11:07:43 +0800

    '入口文件中添加路由'

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,5 +1,5 @@
 <?php
-namespace controllres;
+namespace controllers;
 
 // 引入模型类
 use models\User;
@@ -10,11 +10,16 @@ class UserController
     {
         // 取数据
         $user = new User;
-        $user = $user->getName();
+        $name = $user->getName();
 
         // 加载视图
         view('users.hello', [
             'name' => $name
         ]);
+    }
+
+    public function world()
+    {
+        echo 'world';
     }
 }
