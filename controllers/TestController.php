5fd272b387ad695680aaad0b692d1e8f83b5af41
sunwei <sunwei@czxy.com>
2018-09-04 17:01:16 +0800

    '下载predis扩展并使用'

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -1,12 +1,18 @@
 <?php
 namespace controllers;
 
-class TestController {
+class TestController 
+{
+    public function testRedis()
+    {
+        // 连接 Redis
+        $client = new \Predis\Client([
+            'scheme' => 'tcp',
+            'host'   => '127.0.0.1',
+            'port'   => 32768,
+        ]);
 
+        // $client->set('name','tom');
+        echo $client->get('name');
+    }
 }
-
-
-// 发邮件
-// $redis = new \Predis\Client({
-//     'scheme' => 'tcp'
-// })
--- public/index.php
+++ public/index.php
@@ -2,6 +2,9 @@
 // 定义常量
 define('ROOT', dirname(__FILE__) . '/../');
 
+// 引入 composer 自动加载文件
+require(ROOT.'vendor/autoload.php');
+
 // 实现类的自动加载
 function autoload($class)
 {
@@ -11,25 +14,34 @@ function autoload($class)
 }
 spl_autoload_register('autoload');
 
-// 添加路由 ：解析 URL 上的路径： 控制器/方法
-// 获取 URL 上的路径
+// 添加路由 ：解析 URL 浏览器上 blog/index CLI中就是 blog index
 
-if( isset($_SERVER['PATH_INFO']) )
+if(php_sapi_name() == 'cli')
 {
-    $pathInfo = $_SERVER['PATH_INFO'];
-    // 根据 / 转成数组
-    $pathInfo = explode('/', $pathInfo);
-
-    // 得到控制器名和方法名 ：
-    $controller = ucfirst($pathInfo[1]) . 'Controller';
-    $action = $pathInfo[2];
+    $controller = ucfirst($argv[1]) . 'Controller';
+    $action = $argv[2];
 }
 else
 {
-    // 默认控制器和方法
-    $controller = 'IndexController';
-    $action = 'index';
+    if( isset($_SERVER['PATH_INFO']) )
+    {
+        $pathInfo = $_SERVER['PATH_INFO'];
+        // 根据 / 转成数组
+        $pathInfo = explode('/', $pathInfo);
+
+        // 得到控制器名和方法名 ：
+        $controller = ucfirst($pathInfo[1]) . 'Controller';
+        $action = $pathInfo[2];
+    }
+    else
+    {
+        // 默认控制器和方法
+        $controller = 'IndexController';
+        $action = 'index';
+    }
 }
+
+
 
 // 为控制器添加命名空间
 $fullController = 'controllers\\'.$controller;
